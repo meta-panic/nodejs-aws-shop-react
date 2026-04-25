054b9124a9245413af31e873739cc52e0b0a3e5b
meta-panic <ritaritarita2013@gmail.com>
2026-04-25 17:25:57 +0400

    feat: change header text

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Rita's store of griffons
           </Link>
         </Typography>
 
